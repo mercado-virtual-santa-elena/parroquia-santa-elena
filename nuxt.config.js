2f4c59cb29740a8a9f1b3f1652a5e4fb85da6cc1
mercado-virtual-santa-elena <marbellaisolieth@yahoo.com>
2020-07-23 12:34:05 -0600

    Se agregaron scripst para generar, construir y desplegar la app y se configuro ruta base para github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'server',
+  target: 'static',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
@@ -58,5 +58,8 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+  },
+  router: {
+    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/parroquia-santa-elena/' : ''
   }
 }
